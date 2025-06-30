72b3d04cd60cbe58980a8b4819217a5310cd16c9
Minh Quang <quangpro107@gmail.com>
2025-06-30 11:26:30 +0700

    Update blog

--- Smmsbe.Services/Enum/BlogCategoryType.cs
+++ Smmsbe.Services/Enum/BlogCategoryType.cs
@@ -11,6 +11,7 @@ namespace Smmsbe.Services.Enum
         Nutrition = 1,
         Psychology = 2,
         InfectiousDiseases = 3,
-        Physical = 4
+        Physical = 4,
+        Home = 5
     }
 }
