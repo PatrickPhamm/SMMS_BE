cc57cc2957d734406c7e30a7af75f13cab968324
Minh Quang <quangpro107@gmail.com>
2025-07-01 19:17:46 +0700

    Update enum

--- Smmsbe.Services/Enum/BlogCategoryType.cs
+++ Smmsbe.Services/Enum/BlogCategoryType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Smmsbe.Services.Enum
+﻿namespace Smmsbe.Services.Enum
 {
     public enum BlogCategoryType
     {
@@ -12,6 +6,7 @@ namespace Smmsbe.Services.Enum
         Psychology = 2,
         InfectiousDiseases = 3,
         Physical = 4,
-        Home = 5
+        Home = 5,
+        HealthBenefits = 6
     }
 }
